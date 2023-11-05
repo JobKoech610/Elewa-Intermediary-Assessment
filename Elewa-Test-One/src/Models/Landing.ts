99bf8f64f4376ae9501c1106dc0e8d41529a2056
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 13:15:18 +0300

    :label: feat: Lnading interface

---
+++ Elewa-Test-One/src/Models/Landing.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for  Landing data
+ * @interface
+ * @property {string} scaling - text
+ * @property {string} title - text
+ * @property {string} landingImage - url
+ */
+
+export interface Landing {
+    scaling: string;
+    title: string;
+    landingImage: string;
+  }
