74a4a0b21cc3a3bd48734017bbd9a1074b79f3d3
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 13:18:56 +0300

    :art: feat: useLandingData. fetch data from json API

---
+++ Elewa-Test-One/src/ViewModels/useLandingData.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { Landing } from '../Models/Landing';
+
+export const useLandingData = () => {
+
+    const [LandingData, setLandingData] = useState<Landing | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/landing')
+            .then((response) => response.json())
+            .then((data) => {
+                setLandingData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return LandingData;
+
+}
