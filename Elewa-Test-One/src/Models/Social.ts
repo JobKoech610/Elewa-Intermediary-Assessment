334fa890041b9a4d98a2d15a781fa06182bd527b
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 17:10:14 +0300

    :label: feat social interface

---
+++ Elewa-Test-One/src/Models/Social.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for  Social data
+ * @interface
+ * @property {string} socialImageCard - image url
+ * @property {string} socialHeading - text
+ * @property {string} socialText - Text
+ */
+
+export interface Social {
+    socialImageCard: string;
+    socialHeading: string;
+    socialText: string;
+  }
