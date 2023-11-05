6617290df50afbdf4f6136653a60be6100c81ceb
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 14:19:57 +0300

    :label: feat: Analysis interface

---
+++ Elewa-Test-One/src/Models/Analysis.ts
@@ -0,0 +1,25 @@
+
+/**
+ * Interface for  Analysis data
+ * @interface
+ * @property {string} peopleTrained - text
+ * @property {string} careersLaunched - text
+ * @property {string} projectsFinished - text
+ * @property {string} investors - text
+ * @property {string} peopleTrainedText - text
+ * @property {string} careersLaunchedText - text
+ * @property {string} projectsFinishedText - text
+ * @property {string} investorsText - text
+ */
+
+export interface Analysis {
+    peopleTrained: string;
+    careersLaunched: string;
+    projectsFinished: string;
+    investors: string;
+    peopleTrainedText: string;
+    careersLaunchedText: string;
+    projectsFinishedText: string;
+    investorsText: string;
+
+  }
