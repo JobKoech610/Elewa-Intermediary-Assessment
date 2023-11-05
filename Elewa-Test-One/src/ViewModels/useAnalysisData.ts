5e04da798d7edd2cb308c0c00f57942385f6e138
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 14:24:43 +0300

    :art: feat: useAnalysisData fetch data from json API

---
+++ Elewa-Test-One/src/ViewModels/useAnalysisData.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { Analysis } from '../Models/Analysis';
+
+export const useAnalysisData = () => {
+
+    const [AnalysisData, setAnalysisData] = useState<Analysis | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/analysis')
+            .then((response) => response.json())
+            .then((data) => {
+                setAnalysisData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return AnalysisData;
+
+}
