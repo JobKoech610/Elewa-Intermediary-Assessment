52f4e7ef6c567c9597d852b9ee994bb5bf8b9ab2
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 11:37:31 +0300

    :art: feat:Header component with links and logo

---
+++ Elewa-Test-One/src/Views/Header.tsx
@@ -0,0 +1,40 @@
+import { useHeaderData } from '../ViewModels/useHeaderData';
+import { Link } from "react-router-dom";
+import '../Styles/Header.css'
+const Header: React.FC = () => {
+  const headerData = useHeaderData();
+  console.log(headerData);
+
+  let links;
+  if (headerData) {
+    links = headerData.links.map((link, index) => (
+      <li key={index} className='nav-item'>
+        <Link to={link.url}>{link.text}</Link>
+      </li>
+    ));
+  }
+  
+  return (
+    <header>
+      {headerData ? (
+        <>
+          <nav className='navbars'>
+            <div className='border light-border'>
+              <div className='elewa-text'>
+                <a href="#" className='home-logo'><img src={headerData.logo} alt="logo" /></a>
+              </div>
+              <ul className='navmenu'>
+                {links}
+              </ul>
+              
+            </div>
+          </nav>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </header>
+  );
+};
+
+export default Header;
