1cec76a8ef19e613b4ac80814f7d827f7a947022
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 19:23:42 +0300

    :art: feat: Beyond.tsx jsx elements

---
+++ Elewa-Test-One/src/Views/Beyond.tsx
@@ -0,0 +1,36 @@
+
+import { useBeyondData } from '../ViewModels/useBeyondData';
+import "../Styles/Beyond.css"
+
+const Beyond: React.FC = () => {
+  const BeyondData = useBeyondData();
+  console.log(BeyondData);
+
+  return (
+    <div>
+      {BeyondData ? (
+        <>
+        <div className='beyond-content'>
+        <div className='beyond-image-text'>
+                <div className='beyond-content-image-text'>
+                    <div className="beyond-card-image">
+                        <img src={BeyondData.beyondImageCard} alt="socialImageCard" />
+                    </div>
+                    <div className='beyond-social-text'>
+                        <h1>{BeyondData.beyondHead}</h1>
+                        <p>{BeyondData.beyondText}</p>
+                    </div>
+                    
+                </div>
+          </div>
+
+        </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Beyond;
