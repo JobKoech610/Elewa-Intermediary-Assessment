43ec9f85945db7874b6a1d2589f8681c87a75c91
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 23:09:28 +0300

    :art: feat: useProjectData fecth data from json Api

---
+++ Elewa-Test-One/src/ViewModels/useProjectData.ts
@@ -0,0 +1,23 @@
+
+import { useState, useEffect } from 'react';
+import { Project } from '../Models/Project';
+
+export const useProjectData = () => {
+
+    const [ProjectData, setProjectData] = useState<Project | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/projects')
+            .then((response) => response.json())
+            .then((data) => {
+                setProjectData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return ProjectData;
+
+}
