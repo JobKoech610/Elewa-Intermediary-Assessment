956331fe49687761192029fe72c5f9c9b65fcb39
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 23:06:32 +0300

    :label: feat: project  interface

---
+++ Elewa-Test-One/src/Models/Project.ts
@@ -0,0 +1,17 @@
+/**
+ * Interface for  Header data
+ * @interface
+ * @property {string} projectsHead - Logo
+ * @property {string} projectsText - string
+ * @property {string[]} projectLogos - this is an array containing logo urls)
+
+ */
+
+export  interface Project {
+    projectsHead: string;
+    projectsText: string;
+    projectLogos: string[];
+  }
+  
+  
+  
