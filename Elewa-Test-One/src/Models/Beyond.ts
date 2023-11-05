e64d847eae8b8bb892ca07f17cf0f1f83c819b73
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 19:16:46 +0300

    :label: feat: Beyond interface type

---
+++ Elewa-Test-One/src/Models/Beyond.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for  beyond data
+ * @interface
+ * @property {string} beyondImageCard - image url
+ * @property {string} beyondHead - text
+ * @property {string} beyondText - Text
+ */
+
+export interface Beyond {
+    beyondImageCard: string;
+    beyondHead: string;
+    beyondText: string;
+  }
