0f359516dd4a08964925a05310880b748b54291d
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 19:26:32 +0300

    :art: feat: Open component with jsx elements

---
+++ Elewa-Test-One/src/Views/Open.tsx
@@ -0,0 +1,34 @@
+
+import { useOpenData } from '../ViewModels/useOpenData';
+import "../Styles/Open.css"
+
+const Open: React.FC = () => {
+  const OpenData = useOpenData();
+  console.log(OpenData);
+
+  return (
+    <div>
+      {OpenData ? (
+        <>
+          <div className='open-image-text'>
+                <div className='open-content-image-text'>
+                    <div className="open-card-image">
+                        <img src={OpenData.openImageCard} alt="socialImageCard" />
+                    </div>
+                    <div className='open-social-text'>
+                        <h1>{OpenData.openHead}</h1>
+                        <p>{OpenData.openText}</p>
+                    </div>
+                    
+                </div>
+          </div>
+          
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Open;
