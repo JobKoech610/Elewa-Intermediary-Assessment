d9d8def9732647a85e326ebed56f04b48a247021
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 11:25:47 +0300

    :art: feat: App component with routes for nav

--- Elewa-Test-One/src/App.tsx
+++ Elewa-Test-One/src/App.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SocialImpact from "./Pages/SocialImpact";
+import About from "./Pages/About";
+import Invest from "./Pages/Invest";
+import Header from './Views/Header';
 
-
+// Defining routes and their components and rendering 
 function App() {
   return (
-   <div></div>
+    <Router>
+      <div className="App">
+      <Header />
+        <Routes>
+          <Route path="/social-impact" element={<SocialImpact />} />  
+          <Route path="/about" element={<About />} />
+          <Route path="/invest" element={<Invest />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
