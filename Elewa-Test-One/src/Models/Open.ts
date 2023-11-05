aa9eae34e5c711f3d9a8b61b24b192a4cefc471e
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 19:18:09 +0300

    :label: feat: Open interface type

---
+++ Elewa-Test-One/src/Models/Open.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for  Landing data
+ * @interface
+ * @property {string} openImageCard - url
+ * @property {string} openHead - text
+ * @property {string} openText - text
+ */
+
+export interface Open {
+    openImageCard: string;
+    openHead: string;
+    openText: string;
+  }
