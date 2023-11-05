da63ac9b86c6a0779b4697b016ead4c723fe87fc
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 17:14:44 +0300

    :art: feat: useCooperativeData fetch data from json API

---
+++ Elewa-Test-One/src/ViewModels/useCooperativeData.ts
@@ -0,0 +1,22 @@
+import { useState, useEffect } from 'react';
+import { Cooperative } from '../Models/Cooperative';
+
+export const useCooperativeData = () => {
+
+    const [CooperativeData, setCooperativeData] = useState<Cooperative | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/cooperative')
+            .then((response) => response.json())
+            .then((data) => {
+                setCooperativeData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return CooperativeData;
+
+}
