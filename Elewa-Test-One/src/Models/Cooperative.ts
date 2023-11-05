ecab7f5808e1e387c81df3cd821d13fbace92a86
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 17:11:38 +0300

    :label: feat cooperative interface

---
+++ Elewa-Test-One/src/Models/Cooperative.ts
@@ -0,0 +1,13 @@
+/**
+ * Interface for  cooperative data
+ * @interface
+ * @property {string} coopImageCard - image url
+ * @property {string} coopHead - text
+ * @property {string} coopText - Text
+ */
+
+export interface Cooperative {
+    coopImageCard: string;
+    coopHead: string;
+    coopText: string;
+  }
