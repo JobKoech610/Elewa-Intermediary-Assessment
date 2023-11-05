7a8230f404c3c22ccc17159ed46409fbec6e18ee
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 14:27:04 +0300

    :art: feat: Analysis component consumed data from API in the jsx

---
+++ Elewa-Test-One/src/Views/Analysis.tsx
@@ -0,0 +1,39 @@
+import { useAnalysisData } from '../ViewModels/useAnalysisData';
+import "../Styles/Analysis.css"
+
+const Analysis: React.FC = () => {
+  const analysisData = useAnalysisData();
+  console.log(analysisData);
+
+  return (
+    <header>
+      {analysisData ? (
+        <>
+          <div className='analysis'>
+            <div>
+                <h2>{analysisData.peopleTrained}</h2>
+                <p>{analysisData.peopleTrainedText}</p>
+            </div>
+            <div>
+                <h2>{analysisData.careersLaunched}</h2>
+                <p>{analysisData.careersLaunchedText}</p>
+            </div>
+            <div>
+                <h2>{analysisData.projectsFinished}</h2>
+                <p>{analysisData.projectsFinishedText}</p>
+            </div>
+            <div>
+                <h2>{analysisData.investors}</h2>
+                <p>{analysisData.investorsText}</p>
+            </div>
+          </div>
+          
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </header>
+  );
+};
+
+export default Analysis;
