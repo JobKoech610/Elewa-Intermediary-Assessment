b39c2dd918eb06feb655f7717fa57625204d70fe
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 23:08:23 +0300

    :art: feat: useFooterData fecth data from json Api

---
+++ Elewa-Test-One/src/ViewModels/useFooterData.ts
@@ -0,0 +1,24 @@
+
+
+import { useState, useEffect } from 'react';
+import { Footer } from '../Models/Footer';
+
+export const useFooterData = () => {
+
+    const [footerData, setFooterData] = useState<Footer | null>(null);
+
+    useEffect(() => {
+        fetch('http://localhost:3001/footer')
+            .then((response) => response.json())
+            .then((data) => {
+                setFooterData(data);
+                console.log(data);
+            })
+            .catch((error) => {
+                console.error('Error fetching data:', error);
+            });
+    }, []);
+
+    return footerData;
+
+}
