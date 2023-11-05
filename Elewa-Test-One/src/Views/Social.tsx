7b0748e4a875f21a7fa8a4d48abc622aee040577
jobukk <jobukipkurui3843@gmail.com>
2023-11-05 17:17:32 +0300

    :art: feat: Social component

---
+++ Elewa-Test-One/src/Views/Social.tsx
@@ -0,0 +1,32 @@
+import { useSocialData } from '../ViewModels/useSocialData';
+import "../Styles/Social.css"
+
+const Social: React.FC = () => {
+  const socialData = useSocialData();
+  console.log(socialData);
+
+  return (
+    <div>
+      {socialData ? (
+        <>
+          <div className='image-text'>
+                <div className='content-image-text'>
+                    <div className="card-image">
+                        <img src={socialData.socialImageCard} alt="socialImageCard" />
+                    </div>
+                    <div className='social-text'>
+                        <h1>{socialData.socialHeading}</h1>
+                        <p>{socialData.socialText}</p>
+                    </div>
+                    
+                </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
+    </div>
+  );
+};
+
+export default Social;
