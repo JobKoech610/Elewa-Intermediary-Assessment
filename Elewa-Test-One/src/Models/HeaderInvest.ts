0956f4ed2ec77e6de865c4e85ee7dd9751142976
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 20:53:55 +0300

    :label: feat: HeaderInvest Interface types

---
+++ Elewa-Test-One/src/Models/HeaderInvest.ts
@@ -0,0 +1,19 @@
+/**
+ * Interface for  Landing data
+ * @interface
+ * @property {string} head - text
+ * @property {string} title - text
+ * @property {string} backgroundImage - url
+ * @property {string} headTwo - text
+ * @property {string} text - text
+ * @property {string} cardImage - test
+ */
+
+export interface HeaderInvest {
+    head: string;
+    title: string;
+    backgroundImage: string;
+    headTwo:string;
+    text:string;
+    cardImage:string
+  }
