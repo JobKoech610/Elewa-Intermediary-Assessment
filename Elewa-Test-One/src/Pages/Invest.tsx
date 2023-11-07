8271e9cae84c0f08eb1aa197a4439b4949cb65fc
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 21:29:51 +0300

    :art: feat: Invest component renders the  Invest page

--- Elewa-Test-One/src/Pages/Invest.tsx
+++ Elewa-Test-One/src/Pages/Invest.tsx
@@ -1,8 +1,27 @@
-import React from 'react'
+
+import Header from '../Views/Header';
+import Footer from "../Views/Footer";
+import HeaderInvest from '../Views/HeaderInvest';
+import Context from '../Views/Context'
+import KeysFigures from '../Views/KeyFigures';
+import Stakeholder from '../Views/Stakeholder';
+import Creative from '../Views/Creative';
+import Investing from '../Views/Investing';
+import Caption from '../Views/Caption';
 
 function Invest() {
   return (
-    <div>Invest</div>
+    <div>
+      <Header/>
+      <HeaderInvest />
+      <Context />
+      <KeysFigures />
+      <Stakeholder />
+      <Creative />
+      <Investing />
+      <Caption />
+      <Footer/>
+    </div>
   )
 }
 
