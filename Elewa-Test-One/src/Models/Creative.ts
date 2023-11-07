e7d01e950468bca3d7230576e10dabe9d7d56252
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 20:57:00 +0300

    :label: feat: Creative types /interface

---
+++ Elewa-Test-One/src/Models/Creative.ts
@@ -0,0 +1,16 @@
+/**
+ * Interface for  creative data
+ * @interface
+ * @property {string} imageCard - image url
+ * @property {string} textOne - text
+ * @property {string} title - Text
+ * @property {string} textTwo - Text
+ */
+
+export interface Creative {
+    imageCard: string;
+    textOne: string;
+    title: string;
+    textTwo: string;
+
+  }
