4d53132e89872eda5ec3d4e303bfd8793d76b0d0
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 20:58:42 +0300

    :label: feat: Investing types/interface

---
+++ Elewa-Test-One/src/Models/Investing.ts
@@ -0,0 +1,15 @@
+/**
+ * Interface for  Investing data
+ * @interface
+ * @property {string} imageCard - image url
+ * @property {string} title - text
+ * @property {string} textOne - Text
+ * @property {string} textTwo - Text
+ */
+
+export interface Investing {
+    imageCard: string;
+    title: string;
+    textOne: string;
+    textTwo: string;
+  }
