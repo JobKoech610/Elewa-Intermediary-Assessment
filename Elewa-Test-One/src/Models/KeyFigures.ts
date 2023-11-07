e0124c8f75a34bd10f85d3c7e9a3b631531f4ebc
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 20:55:41 +0300

    :label: feat: KeyFigures types and interface

---
+++ Elewa-Test-One/src/Models/KeyFigures.ts
@@ -0,0 +1,41 @@
+/**
+ * Interface for  Keyfigures data
+ * @interface
+ * @property {string} KeyFigures - text
+ * @property {string} cardOneNum - text
+ * @property {string} cardOneText - Text
+ * @property {string} cardTwoNum - Text
+ * @property {string} cardTwoText - Text
+ * @property {string} cardThreeNum - Text
+ * @property {string} cardThreeText - Text
+ * @property {string} cardFourNum - Text
+ * @property {string} cardFourText - Text
+ * @property {string} cardFiveNum - Text
+ * @property {string} cardFiveText - Text
+ * @property {string} cardSixNum - Text
+ * @property {string} cardSixText - Text
+ * @property {string} cardSevenNum - Text
+ * @property {string} cardSevenText - Text
+ * @property {string} cardEightNum - Text
+ * @property {string} cardEightText - Text
+ */
+
+export interface KeyFigures {
+    KeyFigures: string;
+    cardOneNum: string;
+    cardOneText: string;
+    cardTwoNum: string;
+    cardTwoText: string;
+    cardThreeNum: string;
+    cardThreeText: string;
+    cardFourNum: string;
+    cardFourText: string;
+    cardFiveNum: string;
+    cardFiveText: string;
+    cardSixNum: string;
+    cardSixText: string;
+    cardSevenNum: string;
+    cardSevenText: string;
+    cardEightNum: string;
+    cardEightText: string;  
+  }
