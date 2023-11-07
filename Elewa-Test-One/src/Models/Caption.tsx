1bf66a9678b52c0ba42eab90450977335f93b155
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 21:00:35 +0300

    :label: feat: Caption types/interface

---
+++ Elewa-Test-One/src/Models/Caption.tsx
@@ -0,0 +1,12 @@
+/**
+ * Interface for  Caption data
+ * @interface
+ * @property {string} captionOne - Text
+ * @property {string} captionTwo - Text
+ */
+
+export interface Caption {
+    captionOne: string;
+    captionTwo: string
+    
+  }
