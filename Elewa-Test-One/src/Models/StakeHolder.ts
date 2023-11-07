80de5d9aa6a8098cc5813e695af5b63002ac9d08
jobukk <jobukipkurui3843@gmail.com>
2023-11-07 21:01:52 +0300

    :label: feat: Stakeholder types/interface

---
+++ Elewa-Test-One/src/Models/StakeHolder.ts
@@ -0,0 +1,15 @@
+/**
+ * Interface for  stakeholder data
+ * @interface
+ * @property {string} imageCard - image url
+ * @property {string} textTwo - text
+ * @property {string} textOne - Text
+ * @property {string} title - Text
+ */
+
+export interface Stakeholder {
+    imageCard: string;
+    textTwo: string;
+    textOne: string;
+    title: string;
+  }
